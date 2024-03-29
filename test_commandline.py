f4167de58a86d7923f38cf95cb84b3edfad8b556
tu.lethanh <tu.lethanh@SAIWKS2114>
2024-03-29 18:08:21 +0700

    +added default value for command line

--- test_commandline.py
+++ test_commandline.py
@@ -3,8 +3,8 @@ import argparse
 def init_argparser():
     parser = argparse.ArgumentParser(description= "Testing command line")
 
-    parser.add_argument("dest_dir")
-    parser.add_argument("-o", "--output")
+    parser.add_argument("dest_dir", default="exercises")
+    parser.add_argument("-o", "--output", default="scoring table.json")
 
     parser.parse_args()
     return parser
