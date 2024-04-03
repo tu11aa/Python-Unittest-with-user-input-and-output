886943bc6f81c2e05b20d3fc0f4cd6fdbb7a33eb
Tu <thanhtuhr01@gmail.com>
2024-04-03 22:15:04 +0700

    added more testcases

--- scoring.py
+++ scoring.py
@@ -8,7 +8,44 @@ class CustomTestCase(TestCases):
         super().__init__()
 
     def test_Exercise_1(self):
-        return [TestCase([], [1, 2], isIn=["3"])]
+        return [
+            TestCase([], [1, 2], isContain=["3"])
+        ]
+    
+    def test_Exercise_2(self):
+        return [
+            TestCase([], [1, 0, -1], isContain=["-1", "1"])
+        ]
+    
+    def test_Exercise_3(self):
+        return [
+            TestCase([], ["anna"], isContain=["palindrome"])
+        ]
+    
+    def test_Exercise_4(self):
+        return [
+            TestCase([], ["Pho", "y", "Bun"], isContain=[7400000, ])
+        ]
+    
+    def test_Exercise_5(self):
+        return [
+            TestCase([], ["Vsmart", 8000000], isContain=[7400000,"Oppo 93", "Vsmart", "Vivo"])
+        ]
+    
+    def test_Exercise_6(self):
+        return [
+            TestCase([], [838790], isContain=[6])
+        ]
+    
+    def test_Exercise_7(self):
+        return [
+            TestCase([], [], isContain=["-1 1 5 8 30 92"])
+        ]
+    
+    def test_Exercise_8(self):
+        return [
+            TestCase([], [5], isContain=["1 1 2 3 5"])
+        ]
 
 def init_argparser():
     parser = argparse.ArgumentParser(description= "Scoring command line")
